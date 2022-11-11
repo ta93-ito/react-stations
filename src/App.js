75f403e910faa7cfaf0c5149b2225cd19054500c
ta93-ito <tkm.ito@outlook.jp>
2022-11-12 01:37:54 +0900

    dog state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import * as React from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState(
+    'https://images.dog.ceo/brleeds/spaniel-brittany/n02101388_6057.jpg',
+  )
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <body>
+      <header>Dogアプリ</header>
+      <h1>犬の画像を表示するアプリです</h1>
+      <img src={dogUrl} />
+    </body>
   )
 }
