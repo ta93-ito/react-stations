0cb39de081f6bf01b3c05933b31cace1b50257a3
ta93-ito <tkm.ito@outlook.jp>
2022-11-12 19:31:17 +0900

    onClick to get random dog

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@
 
 import * as React from 'react'
 import './App.css'
-
+import axios from 'axios'
 /**
  *
  * @type {React.FC}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = React.useState(
-    'https://images.dog.ceo/brleeds/spaniel-brittany/n02101388_6057.jpg',
-  )
+  const firstDogUrl =
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg'
+  const [dogUrl, setDogUrl] = React.useState(firstDogUrl)
+
+  const fetchRandomDogApi = () => {
+    const randomDogApi = 'https://dog.ceo/api/breeds/image/random'
+    var request = new XMLHttpRequest()
+    request.open('GET', randomDogApi, false)
+    request.send(null)
+
+    return JSON.parse(request.response).message
+  }
+
   return (
     <body>
       <header>Dogアプリ</header>
       <h1>犬の画像を表示するアプリです</h1>
       <img src={dogUrl} />
+      <br />
+      <button
+        onClick={() => {
+          setDogUrl(fetchRandomDogApi())
+        }}
+      >
+        更新
+      </button>
     </body>
   )
 }
