ed278247f842dd9a1e8dde9c6a133bfc39f8ccbe
ta93-ito <tkm.ito@outlook.jp>
2022-11-20 04:32:10 +0900

    DogListContainer

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,17 @@
 // DO NOT DELETE
+import React, { useEffect, useState } from 'react'
+
+export const DogListContainer = () => {
+  const DogListUrl = 'https://dog.ceo/api/breeds/list/all'
+  const [breeds, setBreeds] = useState()
+
+  useEffect(() => {
+    fetch(DogListUrl)
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setBreeds(Object.keys(data.message))
+      })
+  }, [])
+}
